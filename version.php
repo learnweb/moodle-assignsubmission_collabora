263513aa4dae645277ca148113761dfcf3becee8
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2019-12-22 18:59:25 +0100

    Fix and bump release name

--- version.php
+++ version.php
@@ -27,4 +27,4 @@ $plugin->version = 2019120701;
 $plugin->requires = 2018051700; // M3.5.
 $plugin->component = 'assignsubmission_collabora';
 $plugin->dependencies = array('mod_collabora' => 2019042800);
-
+$plugin->release = 'v3.8-r2';
